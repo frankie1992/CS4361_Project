a1847621a39ac87ba65e9b5e40005e8cab015f04
francisco <francisco.miranda.92@gmail.com>
2018-10-16 23:22:40 -0500

    Added a ship prototype model

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,26 +5,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.physics.box2d.Box2D;
 
 public class MyGdxGame extends ApplicationAdapter {
+	OrthographicCamera camera;
+	ExtendViewport viewport;
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		Box2D.init();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("ship.gif");
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(800, 600, camera);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,13 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title =(" Asteroids");
+		config.width  = 1280;
+		config.height = 720;
+
+		config.fullscreen = true;
+		config.vSyncEnabled = true;
+
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
